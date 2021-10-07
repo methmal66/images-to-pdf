496bae15e8df2f0c6a5ed42443ebd29107be6c5b
Sanuja Methmal <methmal66@yahoo.com>
2021-10-07 18:17:54 +0530

    clean unnecessary utils

--- test/pdfkit.test.ts
+++ test/pdfkit.test.ts
@@ -1,11 +1,9 @@
 import pdfkit from 'pdfkit'
 import fs from 'fs'
 import path from 'path'
-import { get_file_from } from '../src/utils'
 
 test('combine images into pdf', () => {
    const images_dir = path.join(process.cwd(), 'test', 'images')
-   const get_file = get_file_from(images_dir)
    const stream = fs.createWriteStream(path.join(images_dir, 'output.pdf'))
    const file_names = ['1.png', '2.png', '3.png']
 
@@ -13,9 +11,9 @@ test('combine images into pdf', () => {
    doc.pipe(stream)
 
    file_names
-      .map((name) => get_file(name))
-      .forEach((file) => {
-         doc.image(file.name, 0, 0, {
+      .map((name) => path.join(images_dir, name))
+      .forEach((name) => {
+         doc.image(name, 0, 0, {
             width: 595.28,
             align: 'center',
             valign: 'center',
