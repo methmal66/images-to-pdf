11f015d5826d05d804bb71a53e0b459ffa42bf41
Sanuja Methmal <methmal66@yahoo.com>
2021-11-11 19:41:54 +0530

    update bins

--- bin/test/utils.test.js
+++ bin/test/utils.test.js
@@ -1,9 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("../src/utils");
+const path_1 = __importDefault(require("path"));
 test('display current directory', () => {
     expect(utils_1.get_base_dir()).toEqual('images-to-pdf');
 });
 test('find file names in correct order', () => {
-    expect(utils_1.get_files_in_order('/home/methmal/Programs/node/images-to-pdf/test/images')).toEqual(['1.png', '2.png', '3.png', 'output.pdf']);
+    expect(utils_1.get_files_in_order(path_1.default.join(__dirname, 'images'))).toEqual(['1.png', '2.png', '3.png', 'output.pdf']);
 });
