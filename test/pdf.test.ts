7b34a04c0e498981473088d422c3046f5908b97d
Sanuja Methmal <methmal66@yahoo.com>
2021-11-11 19:37:43 +0530

    resolve absolute path

--- test/pdf.test.ts
+++ test/pdf.test.ts
@@ -1,5 +1,6 @@
 import { create_pdf } from '../src/pdf'
+import path from 'path'
 
 test('combine images into pdf', () => {
-   create_pdf(['1.png', '2.png', '3.png'], '/home/methmal/Programs/node/images-to-pdf/test/images', 'output.pdf')
+   create_pdf(['1.png', '2.png', '3.png'], path.join(__dirname, 'images'), 'output.pdf')
 })
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,9 +1,10 @@
 import { get_base_dir, get_files_in_order } from '../src/utils'
+import path from 'path'
 
 test('display current directory', () => {
    expect(get_base_dir()).toEqual('images-to-pdf')
 })
 
 test('find file names in correct order', () => {
-   expect(get_files_in_order('/home/methmal/Programs/node/images-to-pdf/test/images')).toEqual(['1.png', '2.png', '3.png', 'output.pdf'])
+   expect(get_files_in_order(path.join(__dirname, 'images'))).toEqual(['1.png', '2.png', '3.png', 'output.pdf'])
 })
