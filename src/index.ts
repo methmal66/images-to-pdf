dcb0e9e64863b4ab5db78bc6e5d660cd79af334a
Sanuja Methmal <methmal66@yahoo.com>
2021-11-11 18:26:58 +0530

    complete main logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+#! /usr/bin/env ts-node
+
 import inquirer from 'inquirer'
 import questions from './questions'
+import { get_files_in_order } from './utils'
+import { create_pdf } from './pdf'
 
 inquirer
    .prompt(questions)
-   .then((answers) => console.log(answers))
+   .then((answers) => {
+      const path_to_images = process.cwd()
+      const images = get_files_in_order(path_to_images)
+      create_pdf(images, path_to_images, answers.output_file_name)
+   })
    .catch((error) => console.log(error))
