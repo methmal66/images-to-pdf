ee222a4f6582061f8c5f02c78057a112c5f1b203
Sanuja Methmal <methmal66@yahoo.com>
2021-11-11 18:23:00 +0530

    find all the files in a given path

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,14 @@
 import path from 'path'
+import fs from 'fs'
 
 export const get_base_dir = function (): string {
    return path.basename(process.cwd())
+}
+
+export const get_files_in_order = function (dir_path: string): string[] {
+   const files = fs
+      .readdirSync(dir_path, { withFileTypes: true })
+      .filter((item) => item.isFile())
+      .map((file) => file.name)
+   return files.sort()
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,9 @@
-import { get_base_dir } from '../src/utils'
+import { get_base_dir, get_files_in_order } from '../src/utils'
 
 test('display current directory', () => {
    expect(get_base_dir()).toEqual('images-to-pdf')
+})
+
+test('find file names in correct order', () => {
+   expect(get_files_in_order('/home/methmal/Programs/node/images-to-pdf/test/images')).toEqual(['1.png', '2.png', '3.png', 'output.pdf'])
 })
