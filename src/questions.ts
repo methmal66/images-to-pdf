ee0be776c8311a7faaa8df56aaaad3cf06de9301
Sanuja Methmal <methmal66@yahoo.com>
2021-11-11 18:24:31 +0530

    remove question asking for path by using the cwd

--- src/questions.ts
+++ src/questions.ts
@@ -3,12 +3,6 @@ import { get_base_dir } from '../src/utils'
 const questions = [
    {
       type: 'input',
-      name: 'directory',
-      message: 'Path to the directory where images contain',
-      default: '.',
-   },
-   {
-      type: 'input',
       name: 'output_file_name',
       message: 'Output pdf file name',
       default: get_base_dir(),
